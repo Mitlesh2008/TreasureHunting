97e0b67cb31fbf98fe8e279bdbc8b8b2dd8f98d6
Mitlesh <87530002+Mitlesh2008@users.noreply.github.com>
2022-04-26 18:21:27 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "COMPACTDISK";
+const accessCode2 = "CHARLESBABBAGE";
+const accessCode3 = "VACCUMTUBE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("M P C O A D I C S T K", 100,50)
+    fill("lightblue")
+    text("Hint: Full Form Of CD", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("H C L A R B E A S B G A E", 700,150)
+    fill("red")
+    text("Hint: Father Of Computer", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("T V C A U B U E C M", 100,250)
+    fill("orange")
+    text("Hint: First Genration Of Computer", 100,270)
+
+}
